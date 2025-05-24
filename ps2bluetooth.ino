c63de6648bf7e4b446cc7da474fa4c919a49ef4c
Dmitry <metaldr@yandex.ru>
2025-05-24 17:35:17 +0300

    Basic key codes reading from PS/2 pins

--- ps2bluetooth.ino
+++ ps2bluetooth.ino
@@ -1,9 +1,35 @@
+#define DEBUG 1
+#define ARDUINOTRACE_ENABLE DEBUG
+#include <ArduinoTrace.h>
+
+#include <Arduino.h>
+#include <PS2KeyAdvanced.h>
+
+#define PS2_CLK_PIN 14
+#define PS2_DATA_PIN 27
+
+PS2KeyAdvanced keyboard;
+
 void setup()
 {
-	
+    Serial.begin(115200);
+    Serial.println("ESP32 PS/2 Keyboard reader init");
+
+    keyboard.begin(PS2_DATA_PIN, PS2_CLK_PIN);
+
+    Serial.println("Keyboard is ready");
 }
 
 void loop()
 {
-	
+    if (keyboard.available())
+    {
+        int key = keyboard.read();
+    
+
+        DUMP(key);
+
+
+
+    } 
 }
